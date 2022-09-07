22a1cf95243a704192964a608e4d28fc82d4e0c3
sugamypapa <sk000801@naver.com>
2022-09-07 18:46:34 +0900

    Auto stash before checking out "origin/main"

--- practice/src/main/java/jpa/practice/controller/ProductController2.java
+++ practice/src/main/java/jpa/practice/controller/ProductController2.java
@@ -3,7 +3,14 @@ package jpa.practice.controller;
 import jpa.practice.product.Product;
 import jpa.practice.product.ProductService;
 import lombok.RequiredArgsConstructor;
+<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.*;
+=======
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+>>>>>>> Stashed changes
 
 import java.util.List;
 
@@ -14,7 +21,11 @@ public class ProductController2 {
     private final ProductService productService;
 
     @ResponseBody
+<<<<<<< Updated upstream
     @GetMapping(path="/products/list")
+=======
+    @RequestMapping(method = RequestMethod.GET, path = "/products/list")
+>>>>>>> Stashed changes
     public List<Product> list() {
         List<Product> lists = productService.findAll();
 
