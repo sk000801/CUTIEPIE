cf0f88dea79136e1c7c4c669dd8edd5b4f9c5764
sugamypapa <sk000801@naver.com>
2022-10-04 21:26:28 +0900

    fix: for 배포

--- practice/src/main/java/jpa/practice/PracticeApplication.java
+++ practice/src/main/java/jpa/practice/PracticeApplication.java
@@ -18,19 +18,20 @@ public class PracticeApplication extends SpringBootServletInitializer {
 		SpringApplication.run(PracticeApplication.class, args);
 
 		String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; //드라이버
-		String DB_URL = "jdbc:mysql://localhost:3305/CUTIEPIE?&useSSL=false&serverTimezone=UTC"; //접속할 DB 서버
+		String DB_URL = "jdbc:mysql://cutiepie-db.cgua7ml5xlpd.ap-northeast-1.rds.amazonaws.com:3306/cutiepie_db?serverTimezone=UTC&characterEncoding=UTF-8";
 
-		String USER_NAME = "root"; //DB에 접속할 사용자 이름
-		String PASSWORD = "judyserver0801*"; //사용자의 비밀번호
+		String USER_NAME = "admin";
+		String PASSWORD = "judyserver0801*";
+//		String DB_URL = "jdbc:mysql://localhost:3305/CUTIEPIE?&useSSL=false&serverTimezone=UTC"; //접속할 DB 서버
+
+//		String USER_NAME = "root"; //DB에 접속할 사용자 이름
+//		String PASSWORD = "judyserver0801*"; //사용자의 비밀번호
 
 		Connection conn = null;
 
 		try { //Reflection 방식
 				Class.forName(JDBC_DRIVER);
 				conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD); //db 내의 데이터를 저장
-
-
-
 		} catch (ClassNotFoundException | SQLException e) {
 				e.printStackTrace();
 		} finally {
